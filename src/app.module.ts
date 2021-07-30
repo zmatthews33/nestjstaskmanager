2ddc93019594b3d2b7515573b70b3e93ec4701d9
zmatthews33 <zmatt33@gmail.com>
2021-07-30 15:40:55 -0500

    created tasks service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TaskService } from './task/task.service';
 
 @Module({
   imports: [TasksModule],
+  providers: [TaskService],
 })
 export class AppModule {}
