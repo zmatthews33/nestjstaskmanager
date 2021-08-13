6aaa2b16dbb8bf1fd3d3b2d031f89e59430c80c0
zmatthews33 <zmatt33@gmail.com>
2021-08-13 14:35:57 -0500

    added pipe at main.ts level

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
-
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
