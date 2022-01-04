a93e39fe4774892e9ab2acc7d99e848afbdcfb5a
zmatthews33 <zmatt33@gmail.com>
2022-01-04 08:55:20 -0600

    added auth module, service. Added user and user repo

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
+import { UsersRepository } from './users.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersRepository])],
   providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,8 +1,13 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   username: string;
+
+  @Column()
   password: string;
 }
