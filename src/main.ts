d866c05d57fe225fe30b57d42bd5533a2665817d
zmatthews33 <zmatt33@gmail.com>
2022-01-04 19:35:26 -0600

    serialized User Data. Added transform interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transforn.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //validation pipe
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
