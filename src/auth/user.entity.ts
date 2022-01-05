001e9f439a301f9f4096271016dd03be47702c86
zmatthews33 <zmatt33@gmail.com>
2022-01-04 19:21:26 -0600

    made users and tasks relational

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,5 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Task } from '../tasks/task.entity';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
@@ -10,4 +10,7 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany((_type) => Task, (task) => task.user, { eager: true })
+  tasks: Task[];
 }
